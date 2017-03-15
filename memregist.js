e16ff82f314b4ba6778d412dcf25abc52eb06dd0
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-03-15 12:18:18 +0900

    Update

--- memregist.js
+++ memregist.js
@@ -1,45 +1,100 @@
-const Nightmare = require('nightmare');       
-const nightmare = Nightmare({ 
-	show: true,
-	switches : {'ignore-certificate-errors': null}
-});
+const request = require('request');
+const Nightmare = require('nightmare');
 
-nightmare
-  .goto('https://localhost:8443/')
-  .click(".member_link li a[href*=\"/entry\"]")
-  .wait(1)
-  .type("#entry_name_name01","試験")
-  .type("#entry_name_name02","太郎")
-  .type("#entry_kana_kana01", "カタカナ")
-  .type("#entry_kana_kana02", "カタカナ")
-  .type("#zip01","164")
-  .type("#zip02","0001")
-  .click("#zip-search")
-  .wait(1)
-  .type("#addr02","試験1-2-3")
-  .type("#entry_tel_tel01","03")
-  .type("#entry_tel_tel02","1234")
-  .type("#entry_tel_tel03", "1234")
-  .type("#entry_email_first","test0001@nergal.lan")
-  .type("#entry_email_second", "test0001@nergal.lan")
-  .type("#entry_password_first","abcde123")
-  .type("#entry_password_second", "abcde123")
-  .click("button.btn-primary")
-  .wait(1)
-  .pdf("cube.pdf")
-  .click("button.btn-primary")
-  .wait(1)
-  .evaluate(() => {
-      return document.body.innerHTML;
-      //const e1 = document.querySelectorAll('.member_link li');
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+//const HOST = 'localhost';
+const HOST = '192.168.99.100';
 
-      //return e1['0'].href;
-  })
-  .end()
-  .then((result) => {
-      console.log(result);
-  })
-  .catch((error) => {
-      console.error('Search failed:', error);
-  });
+function mailProc() {
 
+    function regist(id) {
+        request(`http://${HOST}:1080/messages/${id}.plain`, (error, response, body) => {
+            const lines = body.split(/\n/);
+            for (let line of lines) {
+                //console.log(line)
+                if (line.startsWith('https') && line.indexOf('/entry/activate/') != -1) {
+                    request(line, (error, response, body) => {
+                        //console.log(error);
+                        //console.log(response);
+                        //console.log(body);
+                    });
+                    console.log(line);
+                }
+            }
+            //parseMail(JSON.parse(body));
+        });
+    }
+
+    function parseMail(json) {
+        for (let i = 0; i < json.length; i++) {
+            //console.log(i,json[i]);
+            if (json[i].recipients[0] != 'admin@example.com') {
+                regist(json[i].id);
+            }
+        }
+    }
+    request(`http://${HOST}:1080/messages`, (error, response, body) => {
+        //console.log(JSON.parse(body));
+        parseMail(JSON.parse(body));
+    })
+}
+
+function preRegist(num) {
+    console.log(`start num = ${num}`);
+    const email = `test002${num}@nergal.lan`;
+
+    const nightmare = Nightmare({
+        show: false,
+        switches: {
+            'ignore-certificate-errors': null
+        }
+    });
+
+    nightmare
+        .goto(`https://${HOST}:8443/`)
+        .click(".member_link li a[href*=\"/entry\"]")
+        .wait(1)
+        .type("#entry_name_name01", `試験`)
+        .type("#entry_name_name02", `太郎`)
+        .type("#entry_kana_kana01", "カタカナ")
+        .type("#entry_kana_kana02", "カタカナ")
+        .type("#zip01", "164")
+        .type("#zip02", "0001")
+        .click("#zip-search")
+        .wait(1)
+        .type("#addr02", "試験1-2-3")
+        .type("#entry_tel_tel01", "03")
+        .type("#entry_tel_tel02", "1234")
+        .type("#entry_tel_tel03", "1234")
+        .type("#entry_email_first", email)
+        .type("#entry_email_second", email)
+        .type("#entry_password_first", "abcde123")
+        .type("#entry_password_second", "abcde123")
+        .click("button.btn-primary")
+        .wait(1)
+        //.pdf("cube.pdf")
+        .click("button.btn-primary")
+        .wait(1)
+        .evaluate(() => {
+            return document.body.innerHTML;
+            //const e1 = document.querySelectorAll('.member_link li');
+
+            //return e1['0'].href;
+        })
+        .end()
+        .then((result) => {
+            //console.log(result);
+            console.log(`${num} done.`);
+            if (num > 0) {
+                preRegist(num - 1);
+            } else {
+                mailProc();
+            }
+        })
+        .catch((error) => {
+            console.error('Search failed:', error);
+        });
+}
+
+preRegist(1000);
+//mailProc();
